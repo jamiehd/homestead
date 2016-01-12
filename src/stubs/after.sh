a69f72db6ad5cb255d0cd2c213de731696b1f440
jamiehd <mail@jamiehd.co.uk>
2016-01-12 10:27:30 +0000

    Added elastic install to after.sh

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-# If you would like to do some extra provisioning you may
-# add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
+add-apt-repository -y ppa:webupd8team/java
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+apt-get -y update
+apt-get -y install oracle-java8-installer
+wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.1.1/elasticsearch-2.1.1.deb
+dpkg -i elasticsearch-2.1.1.deb
+rm elasticsearch-2.1.1.deb
+service elasticsearch restart
